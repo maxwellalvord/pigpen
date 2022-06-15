0c53b34e834a4969710965028d310ca93b16446b
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-15 11:56:03 -0700

    WIP: adding ability to roll two dice
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 function Player() {
   this.score = 0;
   this.lastroll = 0;
+  this.lastroll2 = [];
+  this.lastroll4 = [];
+  // this.newArray = [];
+  // this.newArray2 = [];
 }
 
 // player1.prototype.AddScore = function() {
 //   this.score += getRandomInt(6);
 // }
 
-let newPlayer = new Player(0,0);
-let newPlayer2 = new Player(0,0);
+let newPlayer = new Player(0,0, []);
+let newPlayer2 = new Player(0,0, []);
 
 
 
@@ -20,12 +24,30 @@ Player.prototype.GetInt = function(max) {
     this.lastroll = Math.ceil(Math.random() * 6);
 }
 
+Player.prototype.GetInt2 = function(max) {
+  let newArray = this.lastroll2.push(Math.ceil(Math.random() * 6));
+  let newArray1 = this.lastroll4.push(Math.ceil(Math.random() * 6));
+  console.log('hello');
+}
+
+
+
+Player.prototype.AddScore2 = function(){
+  if ((this.lastroll2.toString() != "1") && (this.lastroll4.toString() != "1")){
+    this.score += (parseInt(this.lastroll2) + parseInt(this.lastroll4));
+  } else if  ((this.lastroll2.toString() != "1") || (this.lastroll4.toString() != "1")){
+    this.score = this.score;
+  } else  {
+    this.score = 0;
+  }
+  
+}
 
 Player.prototype.AddScore = function(){
   if (this.lastroll != 1){
     this.score += this.lastroll;
   } else {
-    this.score = this.score;
+    // this.score = this.score;
   }
   
 }
@@ -52,6 +74,26 @@ $(document).ready(function() {
     
 
   });
+  $("#buttonz").click(function(event) {
+    event.preventDefault();
+    newPlayer.GetInt2();
+    newPlayer.AddScore2();
+    $("#p1score").html(newPlayer.score);
+    console.log(newPlayer);
+    
+
+  });
+
+  $("#buttonx").click(function(event) {
+    event.preventDefault();
+    newPlayer2.GetInt2();
+    newPlayer2.AddScore2();
+    $("#p2score").html(newPlayer2.score);
+    console.log(newPlayer2);
+    
+
+  });
+
   $("#button2").click(function(event) {
     event.preventDefault();
     newPlayer2.GetInt();
@@ -62,5 +104,9 @@ $(document).ready(function() {
   $("#button3").click(function(event) {
     event.preventDefault()
     $("#winner").html(newPlayer.DecideWin(newPlayer.score, newPlayer2.score));
+    newPlayer.score = 0;
+    newPlayer2.score = 0;
+    $("#p1score").html(newPlayer.score);
+    $("#p2score").html(newPlayer2.score);
   });
 });
