87f17261495025bf297e4c7469522e95187a7339
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-15 11:02:29 -0700

    add logic to fix undefined prototypal methods
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,25 +16,51 @@ let newPlayer2 = new Player(0,0);
 //   return Math.ceil(Math.random() * 6);
 // }
 
-Player.prototype.getInt = function(max) {
-  this.lastroll = Math.ceil(Math.random() * 6);
+Player.prototype.GetInt = function(max) {
+    this.lastroll = Math.ceil(Math.random() * 6);
 }
 
 
+Player.prototype.AddScore = function(){
+  if (this.lastroll != 1){
+    this.score += this.lastroll;
+  } else {
+    this.score = this.score;
+  }
+  
+}
+
+Player.prototype.DecideWin = function(num, num1){
+  if (newPlayer.score > newPlayer2.score){
+    return "player one wins";
+  } else if (newPlayer.score < newPlayer2.score){
+    return "player two wins";
+  } else{
+    return "it is a tie, fight to the death"
+  }
+}
 
 
 
 $(document).ready(function() {
   $("#button1").click(function(event) {
     event.preventDefault();
-    newPlayer.getInt();
+    newPlayer.GetInt();
+    newPlayer.AddScore();
+    $("#p1score").html(newPlayer.score);
     console.log(newPlayer);
     
 
   });
   $("#button2").click(function(event) {
     event.preventDefault();
-    newPlayer2.getInt();
+    newPlayer2.GetInt();
+    newPlayer2.AddScore();
+    $("#p2score").html(newPlayer2.score);
     console.log(newPlayer2);
+  });
+  $("#button3").click(function(event) {
+    event.preventDefault()
+    $("#winner").html(newPlayer.DecideWin(newPlayer.score, newPlayer2.score));
   });
 });
