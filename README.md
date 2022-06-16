ccaafaf5ce3e1487c3139941fb7f5ba6114f1f9d
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-16 15:57:04 -0700

    add background css
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-Describe: getRandomInt()
+# PigPen
+By Maxwell Alvord
+
+A simple html page to let a player play pig dice against another player or the computer.
+
+## technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* JQuery
+* MarkDown
+
+## Description
+
+This page will allow the user to decide if they want to play against a person or a computer. Following the decision on who your opponent will be you then decide if you want to roll two dice or one by click either button. When playing against the computer you can manually "hold" to tell the computer it is their turn. After the computers' roll is shown click the judge to see who won!
+
+## Setup/Installation Requirements
+
+* Clone this repository to your desktop.
+* Open portfolio folder.
+* Open index.html
+
+### Alternative Setup
+* Visit the [gh-pages](https://maxwellalvord.github.io/pigpen/)
+
+## Known Bugs
+* Trying to play multiple games without pressing new game button in-between will cause a bug in the logic. Currently no system in place to validate that a new game button has been clicked.
+* Alternating between rolling 1 and 2 dice without clicking new game will cause logic to bug and display incorrect operations.
+* Playing against the computer is currently available for 1 dice only, using two dice will display incorrect operations.
+
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
+
+Copyright &copy;
+2022 Maxwell Alvord
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+## Manual Testing
+```Describe: getRandomInt()
 
 Test: "it should output a number between 1-6"
 Code: 
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 function Player() {
-  this.score = 0;
-  this.lastroll = 0;
-  this.lastroll2 = [];
-  this.lastroll4 = [];
+    this.score = 0;
+    this.lastroll = 0;
+    this.lastroll2 = [];
+    this.lastroll4 = [];
 }
 
 let newPlayer = new Player(0, 0, [], []);
 let newPlayer2 = new Player(0, 0, [], []);
 let newPlayer3 = new Player(0, 0, [], []);
 
-
 Player.prototype.GetInt = function(max) {
     this.lastroll = Math.ceil(Math.random() * 6);
 }
 
 Player.prototype.GetInt2 = function(max) {
-  this.lastroll2.push(Math.ceil(Math.random() * 6));
-  this.lastroll4.push(Math.ceil(Math.random() * 6));
-  console.log(this.lastroll2);
+    this.lastroll2.push(Math.ceil(Math.random() * 6));
+    this.lastroll4.push(Math.ceil(Math.random() * 6));
+    console.log(this.lastroll2);
 }
 
 Player.prototype.getMatch = function(array2, array4) {
@@ -33,198 +32,110 @@ Player.prototype.getMatch = function(array2, array4) {
     }
 }	
 
-// var arr = ['1','2','3'];
-// arr = arr.map(x => parseInt(x));
-
-// console.log(arr);
-//Array(3) [ 1, 2, 3 ]
-
-
-
-// dataSum += parseFloat(data[i]);
-
-// var data = [];
-// data = dataString.split(",");
-
-// return "100,200,300,400,500".split(/,/).map(parseFloat).reduce(function(x,y){return x+y;})
-
-// function removeText() {
-//   $("#p1score2").empty();
-//   $("#p1score2").text(" ");
-// }
-
-
-
-
-// Player.prototype.AddScore2 = function(){
-//   for (let i = this.lastroll2.length - 1; i >= 0; i--) {
-//   if ((this.lastroll2 !== 1) && this.lastroll4 !== 1){
-//     let result =  $("#p1score2").text("keep rolling");
-//     return result;
-//   } else if ((this.lastroll2 === 1) || (this.lastroll4 === 1)){
-//     // let a = $("#p1score2").text("");
-//     $("#p1score2").removeText(empty());
-//     let result = $("#p1score2").text("you got one 1, no points for you!");
-//     return result;
-//   } else  {
-//     let result = $("#p1score2").text("uh no theres goes all your points...");
-//     return result;
-//   }
-// }
-// }
-
-// function test (){
-//   if (('2' !== "1") && '4' !== "1"){
-//       return  "keep rolling";
-//     } else if (('2' === "1") || ('4' === "1")){
-//       // let a = $("#p1score2").text("");
-//       let b = "you got one 1, no points for you!";
-//       return b;
-//     } else  {
-//       return "uh no theres goes all your points...";
-//     }
-//   }
-//   undefined
-//   test()
-//   'keep rolling'
-//   function test (){
-//   if (('1' !== "1") && '1' !== "1"){
-//       return  "keep rolling";
-//     } else if (('2' === "1") || ('4' === "1")){
-//       // let a = $("#p1score2").text("");
-//       let b = "you got one 1, no points for you!";
-//       return b;
-//     } else  {
-//       return "uh no theres goes all your points...";
-//     }
-//   }
-//   undefined
-//   test()
-  
-//   'uh no theres goes all your points...'
-
-// function compMath() {
-//   let rando = (Math.ceil(Math.random() * 6));
-//   console.log(rando);
-//   return rando;
-// }
-
 Player.prototype.GetNew = function(max) {
-  let rando = Math.ceil(Math.random() * 6)
-  if (rando != 1) {
-    this.lastroll = this.lastroll + rando;
-    console.log(this.lastroll);
-  } else {
-    this.lastroll += 0;
-  }
-  let randa = Math.ceil(Math.random() * 6)
-  if (randa != 1) {
-    this.lastroll = this.lastroll + randa;
-    console.log(this.lastroll);
-  } else {
-    this.lastroll += 0;
-  }
+    let rando = Math.ceil(Math.random() * 6)
+    if (rando != 1) {
+      this.lastroll = this.lastroll + rando;
+      console.log(this.lastroll);
+    } else {
+      this.lastroll += 0;
+    }
+    let randa = Math.ceil(Math.random() * 6)
+    if (randa != 1) {
+      this.lastroll = this.lastroll + randa;
+      console.log(this.lastroll);
+    } else {
+      this.lastroll += 0;
+    }
 }
 
-
-
-
-
-
-
-
 Player.prototype.AddScore = function(){
-  if (this.lastroll != 1){
-    this.score += this.lastroll;
-    $("#p1score1").html("")
-  } else {
-    $("#p1score1").html("you got a one");
-  }
-  
+    if (this.lastroll != 1){
+      this.score += this.lastroll;
+      $("#p1score1").html("")
+    } else {
+      $("#p1score1").html("you got a one");
+    }
 }
 
 Player.prototype.DecideWin = function(num, num1, num2){
-  if (num > num || num > num2){
-    return "player one wins";
-  } else if (num < num1 || num < num2){
-    return "player two wins";
-  } else{
-    return "it is a tie, fight to the death"
-  }
+    if (num > num || num > num2){
+      return "player one wins";
+    } else if (num < num1 || num < num2){
+      return "player two wins";
+    } else{
+      return "it is a tie, fight to the death"
+    }
 }
 
-
-
 $(document).ready(function() {
-  // let newPlayer = new Player(0, 0, [], []);
-  // let newPlayer2 = new Player(0, 0, [], []);
-  $("#button1").click(function(event) {
-    event.preventDefault();
-    newPlayer.GetInt();
-    newPlayer.AddScore();
-    $("#p1score").html(newPlayer.score);
-    console.log(newPlayer);
-  });
+    $("#button1").click(function(event) {
+      event.preventDefault();
+      newPlayer.GetInt();
+      newPlayer.AddScore();
+      $("#p1score").html(newPlayer.score);
+      console.log(newPlayer);
+    });
 
-  $("#buttonz").click(function(event) {
-    event.preventDefault();
-    newPlayer.GetInt2();
-    newPlayer.getMatch(newPlayer.lastroll2, newPlayer.lastroll4);
-    // newPlayer.AddScore2(newPlayer.lastroll2, newPlayer2.lastroll4);
-    $("#p1score").html(newPlayer.score);
-    console.log(newPlayer);
-  });
+    $("#buttonz").click(function(event) {
+      event.preventDefault();
+      newPlayer.GetInt2();
+      newPlayer.getMatch(newPlayer.lastroll2, newPlayer.lastroll4);
+      // newPlayer.AddScore2(newPlayer.lastroll2, newPlayer2.lastroll4);
+      $("#p1score").html(newPlayer.score);
+      console.log(newPlayer);
+    });
 
-  $("#buttonx").click(function(event) {
-    event.preventDefault();
-    newPlayer2.GetInt2();
-    newPlayer2.getMatch(newPlayer2.lastroll2,newPlayer2.lastroll4);
-    // newPlayer.AddScore2();
-    $("#p2score").html(newPlayer2.score);
-    console.log(newPlayer2);
-  });
+    $("#buttonx").click(function(event) {
+      event.preventDefault();
+      newPlayer2.GetInt2();
+      newPlayer2.getMatch(newPlayer2.lastroll2,newPlayer2.lastroll4);
+      // newPlayer.AddScore2();
+      $("#p2score").html(newPlayer2.score);
+      console.log(newPlayer2);
+    });
 
-  $("#button2").click(function(event) {
-    event.preventDefault();
-    newPlayer2.GetInt();
-    newPlayer2.AddScore();
-    $("#p2score").html(newPlayer2.score);
-    console.log(newPlayer2);
-  });
+    $("#button2").click(function(event) {
+      event.preventDefault();
+      newPlayer2.GetInt();
+      newPlayer2.AddScore();
+      $("#p2score").html(newPlayer2.score);
+      console.log(newPlayer2);
+    });
 
-  $("#button3").click(function(event) {
-    event.preventDefault();
-    $("#winner").html(newPlayer.DecideWin(newPlayer.score, newPlayer2.score, newPlayer3.score ));
-    newPlayer.score = 0;
-    newPlayer2.score = 0;
-    newPlayer.lastroll = [];
-    newPlayer2.lastroll = [];
-    newPlayer.lastroll2 = [];
-    newPlayer2.lastroll4 = [];
-    $("#p1score").html(newPlayer.score);
-    $("#p2score").html(newPlayer2.score);
-  });
+    $("#button3").click(function(event) {
+      event.preventDefault();
+      $("#winner").html(newPlayer.DecideWin(newPlayer.score, newPlayer2.score, newPlayer3.score ));
+      newPlayer.score = 0;
+      newPlayer2.score = 0;
+      newPlayer.lastroll = [];
+      newPlayer2.lastroll = [];
+      newPlayer.lastroll2 = [];
+      newPlayer2.lastroll4 = [];
+      $("#p1score").html(newPlayer.score);
+      $("#p2score").html(newPlayer2.score);
+    });
 
-  $("#buttonw").click(function(event) {
-    event.preventDefault()
-    window.location.reload();
-  });
-  
-  $("#tuna").click(function(event) {
-    event.preventDefault()
-    $("#button2").hide();
-    $("#buttonx").hide();
-    $("#buttonHold").show();
-    // $("#button3").hide();
-  });
-  $("#bullshark").click(function(event) {
-    event.preventDefault()
-    window.location.reload();
-  });
-  $("#buttonHold").click(function(event) {
-    event.preventDefault();
-    newPlayer3.GetNew();
-    newPlayer3.AddScore(newPlayer.DecideWin(newPlayer.score, newPlayer3.score));
-    $("#p2score").html(newPlayer3.score);
-  });
+    $("#buttonw").click(function(event) {
+      event.preventDefault()
+      window.location.reload();
+    });
+    
+    $("#tuna").click(function(event) {
+      event.preventDefault()
+      $("#button2").hide();
+      $("#buttonx").hide();
+      $("#buttonHold").show();
+      // $("#button3").hide();
+    });
+    $("#bullshark").click(function(event) {
+      event.preventDefault()
+      window.location.reload();
+    });
+    $("#buttonHold").click(function(event) {
+      event.preventDefault();
+      newPlayer3.GetNew();
+      newPlayer3.AddScore(newPlayer.DecideWin(newPlayer.score, newPlayer3.score));
+      $("#p2score").html(newPlayer3.score);
+    });
 });
