7c01eae9239a678c5fbf41f250dc47901de033c9
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-16 11:05:58 -0700

    WIP: if else logic
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,6 @@ function Player() {
   this.lastroll = 0;
   this.lastroll2 = [];
   this.lastroll4 = [];
-  // this.newArray = [];
-  // this.newArray2 = [];
 }
 
 let newPlayer = new Player(0,0, [], []);
@@ -33,21 +31,70 @@ Player.prototype.getMatch = function(array2, array4) {
     }
 }	
 
-Player.prototype.AddScore2 = function(){
-  if ((this.lastroll2.toString() != "1") && (this.lastroll4.toString() != "1")){
-    this.score += (parseInt(this.lastroll2) + parseInt(this.lastroll4));
-  } else if  ((this.lastroll2.toString() != "1") || (this.lastroll4.toString() != "1")){
-    this.score = this.score;
-  } else  {
-    this.score = 0;
-  }
+function removeText() {
+  $("#p1score2").empty();
+  $("#p1score2").text(" ");
 }
+
+Player.prototype.AddScore2 = function(){
+    result = $("#p1score2").text(" ");
+  if ((this.lastroll2.toString() !== "1") && (this.lastroll4.toString() !== "1")){
+    result =  $("#p1score2").text("keep rolling");
+  } else if ((this.lastroll2.toString() === "1") || (this.lastroll4.toString() === "1")){
+    // let a = $("#p1score2").text("");
+    result = $("#p1score2").text("you got one 1, no points for you!");
+  } else  {
+    result = $("#p1score2").text("uh no theres goes all your points...");
+  }
+  return result;
+}
+
+
+// function test (){
+//   if (('2' !== "1") && '4' !== "1"){
+//       return  "keep rolling";
+//     } else if (('2' === "1") || ('4' === "1")){
+//       // let a = $("#p1score2").text("");
+//       let b = "you got one 1, no points for you!";
+//       return b;
+//     } else  {
+//       return "uh no theres goes all your points...";
+//     }
+//   }
+//   undefined
+//   test()
+//   'keep rolling'
+//   function test (){
+//   if (('1' !== "1") && '1' !== "1"){
+//       return  "keep rolling";
+//     } else if (('2' === "1") || ('4' === "1")){
+//       // let a = $("#p1score2").text("");
+//       let b = "you got one 1, no points for you!";
+//       return b;
+//     } else  {
+//       return "uh no theres goes all your points...";
+//     }
+//   }
+//   undefined
+//   test()
+  
+//   'uh no theres goes all your points...'
+
+
+
+
+
+
+
+
+
 
 Player.prototype.AddScore = function(){
   if (this.lastroll != 1){
     this.score += this.lastroll;
+    $("#p1score1").html("")
   } else {
-    // this.score = this.score;
+    $("#p1score1").html("you got a one");
   }
   
 }
@@ -70,26 +117,23 @@ $(document).ready(function() {
     newPlayer.GetInt();
     newPlayer.AddScore();
     $("#p1score").html(newPlayer.score);
-
     console.log(newPlayer);
-    
-
   });
+
   $("#buttonz").click(function(event) {
     event.preventDefault();
     newPlayer.GetInt2();
-    newPlayer.getMatch(newPlayer.lastroll2,newPlayer.lastroll4);
+    newPlayer.getMatch(newPlayer.lastroll2, newPlayer.lastroll4);
+    newPlayer.AddScore2(newPlayer.lastroll2, newPlayer2.lastroll4);
     $("#p1score").html(newPlayer.score);
-
     console.log(newPlayer);
-    
-
   });
 
   $("#buttonx").click(function(event) {
     event.preventDefault();
     newPlayer2.GetInt2();
     newPlayer2.getMatch(newPlayer2.lastroll2,newPlayer2.lastroll4);
+    newPlayer.AddScore2();
     $("#p2score").html(newPlayer2.score);
     console.log(newPlayer2);
   });
@@ -99,11 +143,11 @@ $(document).ready(function() {
     newPlayer2.GetInt();
     newPlayer2.AddScore();
     $("#p2score").html(newPlayer2.score);
-
     console.log(newPlayer2);
   });
+
   $("#button3").click(function(event) {
-    event.preventDefault()
+    event.preventDefault();
     $("#winner").html(newPlayer.DecideWin(newPlayer.score, newPlayer2.score));
     newPlayer.score = 0;
     newPlayer2.score = 0;
@@ -113,12 +157,20 @@ $(document).ready(function() {
     newPlayer2.lastroll4 = [];
     $("#p1score").html(newPlayer.score);
     $("#p2score").html(newPlayer2.score);
-    
   });
+
   $("#buttonw").click(function(event) {
     event.preventDefault()
     window.location.reload();
-   
-    
+  });
+  
+  $("#buttonp").click(function(event) {
+    event.preventDefault()
+    $("#button2").hide();
+    $("#buttonx").hide();
+  });
+  $("#buttonh").click(function(event) {
+    event.preventDefault()
+    window.location.reload();
   });
 });
